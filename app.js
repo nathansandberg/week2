e3cc99ddfec425319fb8740daa40ad3b14e4cfd4
Nathan Sandberg <nathansandberg@gmail.com>
2018-02-07 16:50:17 -0800

    table populated, constructor functions built

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const times = ['6am: ', '7am: ', '8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ','8pm: ', 'Total daily cookies: '];
+const times = ['Store','6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Daily Total'];
 
 
 
@@ -14,55 +14,81 @@ function Stores (name, minHourlyCust, maxHourlyCust, avgCookCust) {
 
 }
 
-Stores.prototype.updatePage = function () {
-    const nathan = document.getElementById(this.name);
-    for (let i = 0; i < this.cookiesArray.length; i++) {
-        const newLi = document.createElement('li');
-        newLi.textContent = times[i] + this.cookiesArray[i];
-        console.log('This is' + this.newLi);
-        nathan.appendChild(newLi);
-    };
-},
-Stores.prototype.totalDailyCookies = function() {
-    let sum = 0;
-    for (let i = 0; i < this.cookiesArray.length; i++){
-        sum += this.cookiesArray[i];
-    }
-    this.cookiesArray.push(sum);
-},
-
 Stores.prototype.randHourlyCust = function() {
     const minimum = Math.ceil(this.minHourlyCust);
     const maximum = Math.floor(this.maxHourlyCust);
-    for (let i = 0; i < 15; i++) {
-        const random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-        const cooky = (random * this.avgCookCust);
-        this.cookiesArray.push(Math.round(cooky));
+    for (let i = 0; i < 16; i++) {
+        if (i < 1){
+            this.cookiesArray.push(this.name);
+        } else{
+            const random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+            const cooky = (random * this.avgCookCust);
+            this.cookiesArray.push(Math.round(cooky));
+        }
     }
 };
 
-const airport = new Stores('airport', 23, 65, 6.3);
-const pioneerSquare = new Stores('pioneer', 3, 24, 1.2);
-const powellsStore = new Stores('powells', 11, 38, 3.7);
-const stJohnsStore = new Stores('stjohns', 20, 38, 2.3);
-const waterFront = new Stores('waterfront', 2, 16, 4.6);
+Stores.prototype.totalDailyCookies = function() {
+    let sum = 0;
+    for (let i = 1; i < this.cookiesArray.length; i++){
+        sum += this.cookiesArray[i];
+    }
+    this.cookiesArray.push(sum);
+
+};
+
+Stores.prototype.renderRow = function(){
+    const table = document.getElementById('table');
+    const row = document.createElement('tr');
+    table.appendChild(row);
+    for (let i = 0; i < this.cookiesArray.length; i++){
+        const cell = document.createElement('td');
+        cell.textContent = this.cookiesArray[i];
+        row.appendChild(cell);
+    }
+};
+
+const airport = new Stores('Airport', 23, 65, 6.3);
+const pioneerSquare = new Stores('Pioneer Place', 3, 24, 1.2);
+const powellsStore = new Stores('Powells', 11, 38, 3.7);
+const stJohnsStore = new Stores('St.johns', 20, 38, 2.3);
+const waterFront = new Stores('Waterfront', 2, 16, 4.6);
+
+function headMake() {
+    const header = document.getElementById('header');
+    const headerRow = document.createElement('tr');
+    header.appendChild(headerRow);
+    for(let i = 0; i < times.length; i++){
+        const headerCell = document.createElement('td');
+        headerCell.textContent = times[i];
+        headerRow.appendChild(headerCell);
+    }
+};
 
 
 airport.randHourlyCust();
 airport.totalDailyCookies();
-airport.updatePage();
+// airport.updatePage();
+airport.renderRow();
+headMake();
+
+
 pioneerSquare.randHourlyCust();
 pioneerSquare.totalDailyCookies();
-pioneerSquare.updatePage();
+pioneerSquare.renderRow();
+// pioneerSquare.updatePage();
 powellsStore.randHourlyCust();
 powellsStore.totalDailyCookies();
-powellsStore.updatePage();
+powellsStore.renderRow();
+// powellsStore.updatePage();
 stJohnsStore.randHourlyCust();
 stJohnsStore.totalDailyCookies();
-stJohnsStore.updatePage();
+stJohnsStore.renderRow();
+// stJohnsStore.updatePage();
 waterFront.randHourlyCust();
 waterFront.totalDailyCookies();
-waterFront.updatePage();
+waterFront.renderRow();
+// waterFront.updatePage();
 
 console.log('poop');
 
